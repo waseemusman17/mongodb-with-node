55014cfa25a7dfe2e3bd27c9b3088715e2385b53
waseemusman17 <waseem.usman17@gmail.com>
2022-02-07 00:46:50 +0500

    Crete Document or Insert into collection

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 
+// DataBase Connection
 mongoose.connect("mongodb://localhost:27017/testApp",
     {
         useNewUrlParser:true,
@@ -8,3 +9,39 @@ mongoose.connect("mongodb://localhost:27017/testApp",
 )
 .then( () => console.log("Connected Successful..."))
 .catch( (err) => console.log(err));
+
+// Collection Scheme/Structure
+const testScheme = new mongoose.Schema({
+    name: String,
+    type: String,
+    active: Boolean,
+    date:{
+        type: Date,
+        default: Date.now
+    }
+})
+
+
+// Colllection Creation
+const Test = new mongoose.model("Test", testScheme );
+
+
+// Create document insert
+const createDocument = async () => {
+    try{
+        const testData = new Test({
+            name: "Vue Js",
+            type: "Front End",
+            active: true
+        })
+        
+        const testResult = await testData.save();
+        console.log(testResult);
+    }catch(err){
+        console.log(err);
+    }
+
+    
+}
+
+createDocument();
